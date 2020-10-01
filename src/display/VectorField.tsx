a1932eca96960e70ab0eda27058eeec00be569bf
Adam Zawierucha <zawiedev@gmail.com>
2020-09-30 21:40:17 -0700

    field opacity implementation (layer method)

--- src/display/VectorField.tsx
+++ src/display/VectorField.tsx
@@ -6,19 +6,67 @@ import { useScaleContext } from "view/ScaleContext"
 
 export interface VectorFieldProps {
   xy: (x: number, y: number) => [number, number]
+  opacity: (x: number, y: number) => number
   step: number
   color?: string
 }
 
 export type Vector = [number, number]
 
-const VectorField: React.VFC<VectorFieldProps> = ({ xy, step = 1, color = "var(--mafs-fg)" }) => {
+interface Layer {
+  d:string,
+  opacity:number
+}
+
+/**
+ * Generates a list of layers. Each layer will eventually be convereted to a <path> 
+ * with a certain opacity.
+ * 
+ * The higher the opacityGrainularity, the more fidelity you get accross opacities, 
+ * however the more layers you have, the more lag you get.
+ * 
+ * @param opacityGrainularity the granulity of the opacity layers
+ * @returns a list of layers
+ */
+function generateOpacityLayers (opacityGrainularity: number = 5): Layer[] {
+  var layers: Layer[] = []
+  const step = 1/opacityGrainularity;
+  for (var i = 1; i > 0; i-=step) {
+    var layer: Layer ={
+      d: "",
+      opacity: i
+    }
+    layers.push(layer)
+  }
+  return layers
+}
+
+/**
+ * Takes in a pointOpacity (a number) and returns the layer it belongs to from layers.
+ * 
+ * @param layers the layers to catagorize pointOpacity to.
+ * @param pointOpacity the opacity to categorize to a layer.
+ * @return the layer that this opacity value belongs to.
+ */
+function findClosetLayer (layers: Layer[], pointOpacity: number): Layer {
+  var bestLayer: Layer = layers[0];
+  for (let layer of layers){
+    if (layer.opacity>pointOpacity){
+      bestLayer = layer
+    } else {
+      break
+    }
+  }
+  return bestLayer
+}
+
+const VectorField: React.VFC<VectorFieldProps> = ({ xy, opacity = (x,y)=>1, step = 1, color = "var(--mafs-fg)" }) => {
   const { pixelMatrix } = useScaleContext()
   const { xPanes, yPanes } = usePaneContext()
 
-  function fieldForRegion(xMin: number, xMax: number, yMin: number, yMax: number) {
-    let d = ""
+  const layers = generateOpacityLayers();
 
+  function fieldForRegion(xMin: number, xMax: number, yMin: number, yMax: number) {
     for (let x = Math.floor(xMin); x <= Math.ceil(xMax); x += step) {
       for (let y = Math.floor(yMin); y <= Math.ceil(yMax); y += step) {
         const tail: Vector = [x, y]
@@ -36,7 +84,9 @@ const VectorField: React.VFC<VectorFieldProps> = ({ xy, step = 1, color = "var(-
         const left = vec.add(pixelTip, vec.rotate(arrowVector, (5 / 6) * Math.PI))
         const right = vec.add(pixelTip, vec.rotate(arrowVector, -(5 / 6) * Math.PI))
 
-        d +=
+        const trueOpacity = opacity(x,y)
+        var layer = findClosetLayer(layers,trueOpacity)
+        layer.d +=
           ` M ${pixelTail[0]} ${pixelTail[1]}` +
           ` L ${pixelTip[0]} ${pixelTip[1]} ` +
           ` L ${left[0]} ${left[1]} ` +
@@ -44,25 +94,27 @@ const VectorField: React.VFC<VectorFieldProps> = ({ xy, step = 1, color = "var(-
           ` L ${pixelTip[0]} ${pixelTip[1]} `
       }
     }
-
-    return d
   }
-
-  const fields: string[] = []
 
   for (const [xMin, xMax] of xPanes) {
     for (const [yMin, yMax] of yPanes) {
-      fields.push(fieldForRegion(xMin, xMax, yMin, yMax))
+      fieldForRegion(xMin, xMax, yMin, yMax)
     }
-  }
+  } 
 
   return (
     <>
-      {fields.map((d, index) => (
+      {layers.map((layer, index) => (
         <path
-          d={d}
+          d={layer.d}
           key={index}
-          style={{ stroke: color || "var(--mafs-fg)", fill: color || "var(--mafs-fg)" }}
+          style={{ 
+            stroke: color || "var(--mafs-fg)", 
+            fill: color || "var(--mafs-fg)",
+            opacity: layer.opacity,
+            fillOpacity: layer.opacity,
+            strokeOpacity: layer.opacity
+            }}
           strokeLinecap="round"
           strokeLinejoin="round"
         />

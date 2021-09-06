a2b6ec8acc1d641de90fc3ce782afc3a184f2cae
Steven Petryk <petryk.steven@gmail.com>
2021-09-06 13:30:18 -0700

    Remove more webpack, test esbuild in the wild

--- esbuild.js
+++ esbuild.js
@@ -3,11 +3,13 @@ const { nodeExternalsPlugin } = require("esbuild-node-externals")
 
 esbuild.build({
   entryPoints: ["src/index.tsx", "src/index.css"],
+  format: "cjs",
   bundle: true,
   minify: true,
   sourcemap: false,
   outdir: "build",
   logLevel: "info",
   watch: process.env.WATCH === "true",
+  target: ["safari13"],
   plugins: [nodeExternalsPlugin()],
 })

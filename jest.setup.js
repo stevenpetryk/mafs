72d21e6dc6172703540f5866532696b625887194
Steven Petryk <petryk.steven@gmail.com>
2021-09-06 20:27:45 -0700

    Increase threshold

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,10 @@
 /* eslint-disable */
 global.ResizeObserver = require("resize-observer-polyfill")
 
-const { toMatchImageSnapshot } = require("jest-image-snapshot")
+const { toMatchImageSnapshot, configureToMatchImageSnapshot } = require("jest-image-snapshot")
 expect.extend({ toMatchImageSnapshot })
-jest.setTimeout(100000)
+
+configureToMatchImageSnapshot({
+  failureThreshold: 0.01,
+  failureThresholdType: "percent",
+})

a6d28637b04c18857cdca4fbb251bcff69dadc1b
Steven Petryk <petryk.steven@gmail.com>
2021-09-06 21:19:05 -0700

    Give up on docker

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 const tsJestPreset = require("ts-jest/jest-preset")
-const puppeteerPreset = require("jest-puppeteer-docker/jest-preset")
+const puppeteerPreset = require("jest-puppeteer/jest-preset")
 
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {

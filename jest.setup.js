093877595cc52b151bddafbd6127fa075cd25551
Steven Petryk <petryk.steven@gmail.com>
2021-09-06 21:17:35 -0700

    use SSIM

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,10 @@
 /* eslint-disable */
 global.ResizeObserver = require("resize-observer-polyfill")
 
-const { toMatchImageSnapshot } = require("jest-image-snapshot")
+const { toMatchImageSnapshot, configureToMatchImageSnapshot } = require("jest-image-snapshot")
 expect.extend({ toMatchImageSnapshot })
+
+configureToMatchImageSnapshot({
+  comparisonMethod: "ssim",
+  failureThreshold: 0.01,
+})

d45249a9d1428bde7264c00a7455b067c8ca0b06
Steven Petryk <petryk.steven@gmail.com>
2021-09-07 13:59:44 -0700

    Add tests for ParametricFunction

--- src/display/FunctionGraph.test.tsx
+++ src/display/FunctionGraph.test.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import FunctionGraph from "./FunctionGraph"
+import { FunctionGraph, Theme } from ".."
 import renderToImage from "../tests/renderToImage"
 
 describe("<FunctionGraph.OfX />", () => {
@@ -22,10 +22,24 @@ describe("<FunctionGraph.Parametric />", () => {
     expect(
       await renderToImage(
         <>
+          {/* Good defaults test */}
           <FunctionGraph.Parametric
             t={[0, 4 * Math.PI]}
-            xy={(t) => [3 + t * Math.cos(t), 3 + t * Math.sin(t)]}
+            xy={(t) => [7 + (t * Math.cos(t)) / 5, 3 + (t * Math.sin(t)) / 5]}
           />
+
+          {/* Styles and sample rate */}
+          <FunctionGraph.Parametric
+            t={[0, 2 * Math.PI]}
+            xy={(t) => [3 - (t * Math.cos(t)) / 3, 6 - (t * Math.sin(t)) / 3]}
+            style="dashed"
+            color={Theme.blue}
+            weight={5}
+            samples={10}
+          />
+
+          {/* When `t` is a backwards range (higher to lower) */}
+          <FunctionGraph.Parametric t={[5, 0]} xy={(t) => [t, 2 + Math.sin(t)]} color={Theme.red} />
         </>
       )
     ).toMatchImageSnapshot()

f14a1824e017f481f2256cd82e0968a9479733f1
Steven Petryk <petryk.steven@gmail.com>
2021-09-07 19:34:14 -0700

    Add a test for opacity

--- src/display/VectorField.test.tsx
+++ src/display/VectorField.test.tsx
@@ -7,9 +7,19 @@ describe("<VectorField />", () => {
     const a = { x: 5.1, y: 5.1 }
     expect(
       await renderToImage(
-        <>
-          <VectorField xy={(x, y) => [y - a.y - (x - a.x), -(x - a.x) - (y - a.y)]} step={0.5} />
-        </>
+        <VectorField xy={(x, y) => [y - a.y - (x - a.x), -(x - a.x) - (y - a.y)]} step={0.5} />
+      )
+    ).toMatchImageSnapshot()
+  })
+
+  it("Supports opacity functions", async () => {
+    expect(
+      await renderToImage(
+        <VectorField
+          xy={() => [0.5, 0.5]}
+          step={1}
+          xyOpacity={(x, y) => Math.min(Math.sqrt(x ** 2 + y ** 2) / 10, 1)}
+        />
       )
     ).toMatchImageSnapshot()
   })

bb9746bb64d997fe12a8ea9b7e02d9123140b7ec
Steven Petryk <petryk.steven@gmail.com>
2023-01-01 13:42:42 -0800

    Close nav drawer

--- docs/app/guides/sidebar.tsx
+++ docs/app/guides/sidebar.tsx
@@ -1,3 +1,5 @@
+import * as React from "react"
+
 import fancyFx from "../../helpers/fancyFx"
 
 import { Guides } from "./guides"
@@ -23,24 +25,25 @@ function GuidesSidebar() {
     </div>
   ))
 
+  const detailsRef = React.useRef<HTMLDetailsElement>(null)
+
+  function handleClick(event: React.MouseEvent<HTMLDetailsElement>) {
+    if (!detailsRef.current) return
+
+    const links = detailsRef.current.querySelectorAll("a")
+    const linkClicked = Array.from(links).some((link) => link.contains(event.target as Node))
+    if (!linkClicked) return
+
+    if (detailsRef.current?.open) {
+      detailsRef.current.open = false
+    }
+  }
+
   return (
     <nav className="relative">
-      <a
-        href="#main"
-        className={`
-          absolute opacity-0 pointer-events-none
-          focus:pointer-events-auto
-          top-0 left-0 right-0 text-center
-          px-4 py-2 text-sm font-semibold
-          bg-gray-50 dark:bg-slate-800
-          focus:opacity-100
-          z-10
-        `}
-      >
-        Skip to main content
-      </a>
+      <SkipLink />
 
-      <details className="md:hidden space-y-4 border-0">
+      <details ref={detailsRef} onClick={handleClick} className="md:hidden space-y-4 border-0">
         <summary className="text-xl font-bold p-4 cursor-default">All guides</summary>
         <div className="p-4 pt-0 space-y-4">{sectionsElements}</div>
       </details>
@@ -51,3 +54,21 @@ function GuidesSidebar() {
 }
 
 export default GuidesSidebar
+function SkipLink() {
+  return (
+    <a
+      href="#main"
+      className={`
+          absolute opacity-0 pointer-events-none
+          focus:pointer-events-auto
+          top-0 left-0 right-0 text-center
+          px-4 py-2 text-sm font-semibold
+          bg-gray-50 dark:bg-slate-800
+          focus:opacity-100
+          z-10
+        `}
+    >
+      Skip to main content
+    </a>
+  )
+}

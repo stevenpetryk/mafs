5bd9bb905e3d1cd3cab0cb4590fb4b5e89e43dc6
Steven Petryk <petryk.steven@gmail.com>
2023-01-02 15:05:08 -0800

    Simplify line/angle example for ios perf reasons

--- docs/components/guide-examples/LinePointAngleExample.tsx
+++ docs/components/guide-examples/LinePointAngleExample.tsx
@@ -1,23 +1,22 @@
 "use client"
 
-import * as React from "react"
 import {
   Mafs,
   Line,
   CartesianCoordinates,
   useMovablePoint,
-  useStopwatch,
 } from "mafs"
 
 export default function LinePointAngleExample() {
   const point = useMovablePoint([-1, -1])
-  const { time: angle, start } = useStopwatch()
-  React.useEffect(() => start(), [start])
 
   return (
     <Mafs>
       <CartesianCoordinates />
-      <Line.PointAngle point={point.point} angle={angle} />
+      <Line.PointAngle
+        point={point.point}
+        angle={Math.PI / 6}
+      />
       {point.element}
     </Mafs>
   )

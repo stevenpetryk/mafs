1b37df8589d21eb6bd5f0346f0b6c5f8ac00bc94
Steven Petryk <petryk.steven@gmail.com>
2023-01-01 21:49:45 -0800

    Fix homepage riemann scrolling

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -28,7 +28,12 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="homepage-mafs -mt-24 sm:mt-0 col-start-1 row-start-1 -z-10">
+        <div
+          className="homepage-mafs -mt-24 sm:mt-0 col-start-1 row-start-1 touch-none"
+          style={{
+            clipPath: "polygon(0% 68%, 85% 68%, 100% 0%, 100% 100%, 0% 100%)",
+          }}
+        >
           <RiemannHomepage />
         </div>
       </div>

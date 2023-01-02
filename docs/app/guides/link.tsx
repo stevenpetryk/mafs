06eb6bb7fcdb2e9fd70ffce974d14f48967b596f
Steven Petryk <petryk.steven@gmail.com>
2023-01-01 21:38:42 -0800

    Fix mobile nav styling

--- docs/app/guides/link.tsx
+++ docs/app/guides/link.tsx
@@ -19,13 +19,13 @@ export function GuideLink({ sectionTitle, guideTitle, guideSlug, children }: Gui
         href={`/guides/${sectionTitle}/${guideTitle}`}
         className={`
           flex gap-2 items-center px-2 -mx-2 py-0.5 rounded-lg
-          hover:bg-slate-100 dark:hover:bg-slate-800
+          md:hover:bg-slate-100 md:dark:hover:bg-slate-800
           hover:text-blue-700 focus:text-blue-700
           focus:z-10
           dark:text-slate-300 dark:focus:text-blue-300 dark:hover:text-blue-300
           ${
             active
-              ? "text-blue-700 dark:text-blue-400 dark:focus:text-blue-400 dark:hover:text-blue-400 font-bold bg-slate-100 dark:bg-slate-800"
+              ? "text-blue-700 dark:text-blue-400 dark:focus:text-blue-400 dark:hover:text-blue-400 font-bold md:bg-slate-100 md:dark:bg-slate-800"
               : ""
           }`}
       >

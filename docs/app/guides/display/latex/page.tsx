381833d91cb86476db389dfc77444b678ee31410
Steven Petryk <petryk.steven@gmail.com>
2023-01-06 00:50:02 -0800

    add prop table

--- docs/app/guides/display/latex/page.tsx
+++ docs/app/guides/display/latex/page.tsx
@@ -1,5 +1,8 @@
 "use client"
 import CodeAndExample from "components/CodeAndExample"
+import { PropTable } from "components/PropTable"
+
+import { LaTeX } from "mafs"
 
 import LatexExample from "components/guide-examples/display/Latex"
 import LatexExampleSource from "!raw-loader!components/guide-examples/display/Latex"
@@ -10,6 +13,7 @@ import LatexDocExampleSource from "!raw-loader!components/guide-examples/display
 export default function LatexPage() {
   return (
     <>
+      <PropTable of={LaTeX} />
       <CodeAndExample component={<LatexExample />} source={LatexExampleSource} />
       <CodeAndExample component={<LatexDocExample />} source={LatexDocExampleSource} />
     </>

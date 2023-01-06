d3304872ac37b8c9002be706ed638613d5a4bb3a
Steven Petryk <petryk.steven@gmail.com>
2023-01-05 22:57:33 -0800

    Include CMU Serif, simplify CSS imports (#72)
    This uses my new [`computer-modern`
    package](https://npmjs.com/package/computer-modern) to make Computer
    Modern Serif available directly from Mafs.
    
    This changes the CSS import in Mafs:
    
    ```diff
    -@import "mafs/build/index.css";
    +@import "mafs/core.css";
    +@import "mafs/font.css";
    ```
    
    `font.css` is optional. It just imports `computer-modern/cmun-serif.css`
    and sets the MafsView font.
    
    ---
    
    One thing that might be handy in the future would be to subset the fonts
    to only have numbers and common characters, so that the browser doesn't
    have to load so much content just to show axes.

--- docs/app/guides/get-started/installation/page.tsx
+++ docs/app/guides/get-started/installation/page.tsx
@@ -5,6 +5,7 @@ import CodeAndExample from "../../../../components/CodeAndExample"
 
 import Plain from "../../../../components/guide-examples/hello-fx/plain"
 import PlainSource from "!raw-loader!../../../../components/guide-examples/hello-fx/plain"
+import endent from "endent"
 
 function Page() {
   return (
@@ -20,46 +21,39 @@ function Page() {
         `}
       />
 
-      <p>And then make sure to load the stylesheet:</p>
+      <p>And then make sure to load the stylesheet.</p>
 
-      <Code language="css" source={`@import "mafs/build/index.css";`} />
+      <Code
+        language="css"
+        source={endent`
+          @import "mafs/core.css";
+        `}
+      />
 
       <p>Now, in your React app, you should be able to render a Mafs view.</p>
 
       <CodeAndExample component={<Plain />} source={PlainSource} />
 
-      <h2>Vector math</h2>
-
-      <p>
-        Mafs ships with a tiny set of linear algebra functions. You can import everything under
-        <code>vec</code>.
-      </p>
-
-      <Code
-        language="tsx"
-        source={`
-          import { vec } from "mafs"
-          vec.add([1, 2], [2, 3]) // [3, 5]
-        `}
-      />
-
       <h2>Fancy math font</h2>
 
       <p>
-        The font in use on this site—Computer Modern Serif—does not ship with Mafs to avoid
-        licensing problems. Mafs will inherit the body's font. You can{" "}
-        <a href="https://www.checkmyworking.com/cm-web-fonts/">download Computer Modern webfonts</a>{" "}
-        yourself to make it available on the page, then customize the Mafs font like this:
+        The font in use on this site (Computer Modern Serif) can be used with Mafs by importing{" "}
+        <code>mafs/font.css</code>. It will import the appropriate font files and set Mafs'
+        font-family.
       </p>
 
       <Code
         language="css"
         source={`
-          .MafsView {
-            font-family: "Computer Modern Serif", serif;
-          }
+          @import "mafs/core.css";
+          @import "mafs/font.css";
         `}
       />
+
+      <p>
+        Computer Modern looks great, but it comes at a cost: using it will add about 220kB to your
+        page load.
+      </p>
     </>
   )
 }

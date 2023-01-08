bfed6cdeef884f1488262acd3c95fbe1386d102f
Steven Petryk <petryk.steven@gmail.com>
2023-01-07 23:51:44 -0800

    Update types

--- docs/components/guide-examples/plots/sine-stress-test.tsx
+++ docs/components/guide-examples/plots/sine-stress-test.tsx
@@ -2,9 +2,8 @@
 
 import { CartesianCoordinates, Mafs, Plot } from "mafs"
 
+const fn = (x: number) => Math.sin(1 / x)
 export default function SineStressTest() {
-  const fn = (x: number) => Math.sin(1 / x)
-
   return (
     <Mafs
       height={300}

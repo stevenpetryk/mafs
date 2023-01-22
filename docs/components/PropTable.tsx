aa06a2eb66555207a9f2cffbd13867270bd5b2d9
Steven Petryk <petryk.steven@gmail.com>
2023-01-21 19:27:54 -0800

    remove log

--- docs/components/PropTable.tsx
+++ docs/components/PropTable.tsx
@@ -41,8 +41,6 @@ export function PropTable({ of: component }: PropTableProps) {
     throw new Error("Non-docgen object passed to PropTable")
   }
 
-  console.log({ component })
-
   const displayName =
     // eslint-disable-next-line
     (component as any)?.displayName ?? docgenInfo.displayName

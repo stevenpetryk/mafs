c2709b2685eba66239d98e8eef4fe355dc56babc
Steven Petryk <petryk.steven@gmail.com>
2023-01-25 11:42:51 -0800

    Make visual tests more useful

---
+++ src/context/TestContext.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+interface TestContextShape {
+  overrideHeight?: number
+}
+
+export const TestContext = React.createContext<TestContextShape>({
+  overrideHeight: undefined,
+})
+
+export const TestContextProvider = TestContext.Provider

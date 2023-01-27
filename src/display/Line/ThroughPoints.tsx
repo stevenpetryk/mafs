eeba97c76ea1673e30355971967a845cb2c9930b
Steven Petryk <petryk.steven@gmail.com>
2023-01-26 18:20:15 -0800

    Remove jank from Line.ThroughPoints

--- src/display/Line/ThroughPoints.tsx
+++ src/display/Line/ThroughPoints.tsx
@@ -2,6 +2,7 @@ import { Stroked, Theme } from "../../display/Theme"
 import { useTransformContext } from "../../context/TransformContext"
 import { round } from "../../math"
 import { vec } from "../../vec"
+import { usePaneContext } from "../../context/PaneContext"
 
 export interface ThroughPointsProps extends Stroked {
   point1: vec.Vector2
@@ -16,20 +17,39 @@ export function ThroughPoints({
   weight = 2,
   opacity = 1.0,
 }: ThroughPointsProps) {
-  const { viewTransform, userTransform } = useTransformContext()
-  const transform = vec.matrixMult(viewTransform, userTransform)
-  const segment = vec.scale(vec.normalize(vec.sub(point2, point1)), 100000)
+  const { xPaneRange, yPaneRange } = usePaneContext()
+  const [xMin, xMax] = xPaneRange
+  const [yMin, yMax] = yPaneRange
 
-  const scaledPoint1 = vec.transform(vec.sub(point1, segment), transform)
-  const scaledPoint2 = vec.transform(vec.add(point2, segment), transform)
+  const { userTransform } = useTransformContext()
+
+  const tPoint1 = vec.transform(point1, userTransform)
+  const tPoint2 = vec.transform(point2, userTransform)
+
+  const slope = (tPoint2[1] - tPoint1[1]) / (tPoint2[0] - tPoint1[0])
+
+  let offscreen1: vec.Vector2
+  let offscreen2: vec.Vector2
+
+  if (Math.abs(Math.atan(slope)) > Math.PI / 4) {
+    offscreen1 = [xMin, slope * (xMin - tPoint1[0]) + tPoint1[1]]
+    offscreen2 = [xMax, slope * (xMax - tPoint1[0]) + tPoint1[1]]
+  } else {
+    offscreen1 = [(yMin - tPoint1[1]) / slope + tPoint1[0], yMin]
+    offscreen2 = [(yMax - tPoint1[1]) / slope + tPoint1[0], yMax]
+  }
 
   return (
     <line
-      x1={round(scaledPoint1[0], 2)}
-      y1={round(scaledPoint1[1], 2)}
-      x2={round(scaledPoint2[0], 2)}
-      y2={round(scaledPoint2[1], 2)}
-      style={{ stroke: color }}
+      x1={round(offscreen1[0], 2)}
+      y1={round(offscreen1[1], 2)}
+      x2={round(offscreen2[0], 2)}
+      y2={round(offscreen2[1], 2)}
+      style={{
+        stroke: color,
+        transform: "var(--mafs-view-transform)",
+        vectorEffect: "non-scaling-stroke",
+      }}
       strokeWidth={weight}
       opacity={opacity}
       strokeDasharray={style === "dashed" ? "4,3" : undefined}

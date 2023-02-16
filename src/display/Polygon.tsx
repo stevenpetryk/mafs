5c7bd4c2da91f0cd2ed5039f308f82af96cbe237
Koen Klaren <koen.klaren@gmail.com>
2023-02-16 12:52:32 +0100

    Implement Polyline as separate component, but share underlying base component

--- src/display/Polygon.tsx
+++ src/display/Polygon.tsx
@@ -1,47 +1,12 @@
 import * as React from "react"
-import { Theme, Filled } from "./Theme"
-import { useTransformContext } from "../context/TransformContext"
-import { vec } from "../vec"
+import { PolyBase, PolyBaseProps } from "./PolyBase"
 
-export interface PolygonProps extends Filled {
-  points: vec.Vector2[]
+export interface PolygonProps extends PolyBaseProps {
   svgPolygonProps?: React.SVGProps<SVGPolygonElement>
 }
 
-export function Polygon({
-  points,
-  color = Theme.foreground,
-  weight = 2,
-  fillOpacity = 0.15,
-  strokeOpacity = 1.0,
-  strokeStyle = "solid",
-  svgPolygonProps = {},
-}: PolygonProps) {
-  const { userTransform } = useTransformContext()
-
-  const scaledPoints = points
-    .map((point) => vec.transform(point, userTransform).join(" "))
-    .join(" ")
-
-  return (
-    <polygon
-      points={scaledPoints}
-      strokeWidth={weight}
-      fillOpacity={fillOpacity}
-      strokeDasharray={strokeStyle === "dashed" ? "4,3" : undefined}
-      strokeLinejoin="round"
-      {...svgPolygonProps}
-      style={{
-        fill: color,
-        fillOpacity,
-        stroke: color,
-        strokeOpacity,
-        vectorEffect: "non-scaling-stroke",
-        transform: "var(--mafs-view-transform)",
-        ...(svgPolygonProps.style || {}),
-      }}
-    ></polygon>
-  )
+export function Polygon({ svgPolygonProps, ...otherProps }: PolygonProps) {
+  return <PolyBase element="polygon" svgPolyProps={svgPolygonProps} {...otherProps} />
 }
 
 Polygon.displayName = "Polygon"

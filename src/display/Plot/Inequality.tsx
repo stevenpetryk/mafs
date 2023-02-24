46c46f9e1f0eb754d418983ae546d40c11a4f3a6
Steven Petryk <petryk.steven@gmail.com>
2023-02-24 09:43:10 -0800

    more wip

--- src/display/Plot/Inequality.tsx
+++ src/display/Plot/Inequality.tsx
@@ -9,12 +9,7 @@ const enum BoundType {
   INEQUAL,
 }
 
-type Fn = (x: number) => number
-
 interface BaseInequalityProps {
-  /** A function that takes a `t` value and returns a point. */
-  y: { ">"?: Fn; "<"?: Fn; ">="?: Fn; "<="?: Fn }
-
   color?: string
   weight?: number
   strokeColor?: string
@@ -46,6 +41,110 @@ export type InequalityOfYProps = BaseInequalityProps & {
 }
 
 export function InequalityOfX({
+  y,
+  color = Theme.foreground,
+  weight = 2,
+  strokeColor = color,
+  strokeOpacity = 1.0,
+  fillColor = color,
+  fillOpacity = 0.15,
+
+  minSamplingDepth = 10,
+  maxSamplingDepth = 14,
+
+  upperColor = strokeColor,
+  upperOpacity = strokeOpacity,
+  upperWeight = weight,
+  lowerColor = strokeColor,
+  lowerOpacity = strokeOpacity,
+  lowerWeight = weight,
+
+  svgUpperPathProps = {},
+  svgLowerPathProps = {},
+  svgFillPathProps = {},
+}: InequalityOfXProps) {
+  const {
+    xPaneRange: [xMin, xMax],
+    yPaneRange: [yMin, yMax],
+  } = usePaneContext()
+
+  let upperBoundType = BoundType.UNBOUNDED
+  if ("<=" in y) upperBoundType = BoundType.EQUAL
+  if ("<" in y) upperBoundType = BoundType.INEQUAL
+
+  let lowerBoundType = BoundType.UNBOUNDED
+  if (">=" in y) lowerBoundType = BoundType.EQUAL
+  if (">" in y) lowerBoundType = BoundType.INEQUAL
+
+  const upperFn = y["<"] ?? y["<="] ?? (() => yMax)
+  const lowerFn = y[">"] ?? y[">="] ?? (() => yMin)
+
+  const svgPath = sampleInequality(
+    upperFn,
+    lowerFn,
+    [xMin, xMax],
+    minSamplingDepth,
+    maxSamplingDepth,
+    0.1
+  )
+
+  return (
+    <g>
+      <path
+        d={svgPath.fill}
+        style={{
+          fill: fillColor || "var(--mafs-fg)",
+          fillOpacity,
+          stroke: "none",
+          transform: "var(--mafs-view-transform)",
+          vectorEffect: "non-scaling-stroke",
+          ...svgFillPathProps?.style,
+        }}
+        {...svgFillPathProps}
+      />
+
+      {upperBoundType != BoundType.UNBOUNDED && (
+        <path
+          d={svgPath.upper}
+          strokeWidth={upperWeight}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeDasharray={upperBoundType === BoundType.INEQUAL ? "4,8" : ""}
+          style={{
+            fill: "none",
+            stroke: upperColor,
+            strokeOpacity: upperOpacity,
+            transform: "var(--mafs-view-transform)",
+            vectorEffect: "non-scaling-stroke",
+            ...svgUpperPathProps?.style,
+          }}
+          {...svgUpperPathProps}
+        />
+      )}
+
+      {lowerBoundType != BoundType.UNBOUNDED && (
+        <path
+          d={svgPath.lower}
+          strokeWidth={lowerWeight}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeDasharray={lowerBoundType === BoundType.INEQUAL ? "4,8" : ""}
+          style={{
+            fill: "none",
+            stroke: lowerColor,
+            strokeOpacity: lowerOpacity,
+            transform: "var(--mafs-view-transform)",
+            vectorEffect: "non-scaling-stroke",
+            ...svgLowerPathProps?.style,
+          }}
+          {...svgLowerPathProps}
+        />
+      )}
+    </g>
+  )
+}
+
+export function Inequality({
   y,
   color = Theme.foreground,
   weight = 2,

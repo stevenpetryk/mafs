a53e378470cc44d622e8ae41fa9cafc4f1792310
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2023-07-25 20:58:42 +0200

    add review suggestions

--- docs/components/guide-examples/examples/PolarCoordinates.tsx
+++ docs/components/guide-examples/examples/PolarCoordinates.tsx
@@ -1,8 +1,15 @@
 import * as React from "react";
-import { Mafs, Line, Coordinates, useMovablePoint, Plot } from "mafs";
+import {
+  Mafs,
+  vec,
+  Theme,
+  Line,
+  Coordinates,
+  useMovablePoint,
+  Plot
+} from "mafs";
 
 export default function PolarCoordinate() {
-  const viewBox = { x: [-2, 2], y: [-4, 4] };
   const point1 = useMovablePoint([-2, -1]);
   const r = vec.mag(point1.point);
   let angle = Math.atan2(point1.point[1], point1.point[0]);
@@ -13,37 +20,35 @@ export default function PolarCoordinate() {
   }
 
   return (
-    <div style={{ display: "flex", justifyContent: "space-between" }}>
-      <div style={{ flex: 1, marginRight: "10px" }}>
-        <Mafs viewBox={viewBox} pan={false}>
-          <Coordinates.Cartesian />
-          <Line.Segment
-            point1={[0, 0]}
-            point2={[point1.point[0], 0]}
-            color="yellow"
-          />
-          <Line.Segment
-            point1={[point1.point[0], 0]}
-            point2={point1.point}
-            color="yellow"
-          />
+    <div
+      style={{ display: "flex", justifyContent: "space-between", gap: "10px" }}
+    >
+      <Mafs viewBox={{ x: [-2, 2], y: [-4, 4] }} pan={false}>
+        <Coordinates.Cartesian />
+        <Line.Segment
+          point1={[0, 0]}
+          point2={[point1.point[0], 0]}
+          color={Theme.blue}
+        />
+        <Line.Segment
+          point1={[point1.point[0], 0]}
+          point2={point1.point}
+          color={Theme.blue}
+        />
 
-          {point1.element}
-        </Mafs>
-      </div>
+        {point1.element}
+      </Mafs>
 
-      <div style={{ flex: 1, marginLeft: "10px" }}>
-        <Mafs viewBox={viewBox} pan={false}>
-          <Coordinates.Polar subdivisions={3} lines={1} />
-          <Line.Segment point1={[0, 0]} point2={[r, 0]} color="yellow" />
-          <Plot.Parametric
-            t={[0, angle]}
-            xy={(t) => [r * Math.cos(t), r * Math.sin(t)]}
-            color={Theme.blue}
-          />
-          {point1.element}
-        </Mafs>
-      </div>
+      <Mafs viewBox={{ x: [-2, 2], y: [-4, 4] }} pan={false}>
+        <Coordinates.Polar subdivisions={3} lines={1} />
+        <Line.Segment point1={[0, 0]} point2={[r, 0]} color={Theme.blue} />
+        <Plot.Parametric
+          t={[0, angle]}
+          xy={(t) => [r * Math.cos(t), r * Math.sin(t)]}
+          color={Theme.blue}
+        />
+        {point1.element}
+      </Mafs>
     </div>
   );
 }

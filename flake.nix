143bbca2ccf478aba444e026032699a3cb61da95
Steven Petryk <petryk.steven@gmail.com>
2024-03-09 22:32:58 -0800

    Add a Nix flake for easy dev parity

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Mafs' developer environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.nodejs_20
+
+            (pkgs.writeScriptBin "pnpm" ''
+              corepack pnpm "$@"
+            '')
+          ];
+        };
+      });
+}

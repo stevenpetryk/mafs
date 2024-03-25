b73308b0dbaadbd4b342d1fb56d6ed775919c68f
Steven Petryk <petryk.steven@gmail.com>
2024-03-24 21:12:11 -0700

    Automatically publish PRs from non-forks
    Collaborators can get a published npm package, as a treat.

---
+++ scripts/publish-branch.ts
@@ -0,0 +1,14 @@
+import childProcess from "node:child_process"
+import fs from "node:fs"
+
+const shortCommit = childProcess.execSync("git rev-parse --short HEAD", { encoding: "utf8" }).trim()
+
+const version = `0.0.0-${shortCommit}`
+
+console.log(`Publishing ${version} to npm...`)
+
+const packageJson = JSON.parse(fs.readFileSync("package.json", "utf8"))
+packageJson.version = version
+fs.writeFileSync("package.json", JSON.stringify(packageJson, null, 2))
+
+childProcess.execSync("npm publish --access public --tag experimental", { stdio: "inherit" })
